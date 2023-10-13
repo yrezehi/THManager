a479cf945039e876f911f78d0e43a0dc31c02f69
unknown <yrezehi@gmail.com>
2023-10-12 22:50:08 -0700

    setup: Worker

---
+++ THManager/Worker.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace THManager
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public readonly string Description;
+        public readonly DateTime CreationTime;
+        public DateTime StartTime { get; set; }
+        public Thread JobThread { get; }
+        public readonly ParameterizedThreadStart JonAction;
+
+        public Worker(string description, ParameterizedThreadStart jobAction, bool triggerImmediately = false)
+        {
+            CreationTime = DateTime.Now;
+            Description = description;
+            JonAction = jobAction;
+
+            JobThread = new Thread(jobAction);
+            Id = JobThread.ManagedThreadId;
+
+            if (triggerImmediately)
+            {
+                StartTime = DateTime.Now;
+                JobThread.Start();
+            }
+        }
+
+        public void Trigger()
+        {
+            if (JobThread.ThreadState == ThreadState.Unstarted)
+            {
+                StartTime = DateTime.Now;
+                JobThread.Start();
+            }
+        }
+    }
+}
